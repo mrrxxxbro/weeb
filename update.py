9c195fd6e546e55ee4307eb9c77e0897feaa8edb
⎠⎠ THE༒KILLER ⎝⎝ <67904798+killerlearner@users.noreply.github.com>
2023-04-18 17:53:05 +0530

    Update update.py

--- update.py
+++ update.py
@@ -52,6 +52,7 @@ if len(UPSTREAM_REPO) == 0:
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
+   UPSTREAM_BRANCH = ''
 
 if UPSTREAM_REPO is not None:
     if ospath.exists('.git'):
